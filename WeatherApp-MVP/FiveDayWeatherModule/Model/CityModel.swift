45957682524629c07af51ac2af44acde358a6536
Mikhail Kosarev <michael.kosarev@gmail.com>
2022-09-29 16:24:31 +0200

    create FiveDayModel

---
+++ WeatherApp-MVP/FiveDayWeatherModule/Model/CityModel.swift
@@ -0,0 +1,17 @@
+//
+//  CityModel.swift
+//  WeatherApp-MVP
+//
+//  Created by Mikhail on 29.09.2022.
+//
+
+import Foundation
+
+struct CityModel {
+    let name: String
+    let country: String
+    
+    var fullCityName: String {
+        "\(name), \(country)"
+    }
+}
