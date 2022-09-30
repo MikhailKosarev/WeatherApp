aa04cb6255df102bd851c98368a5c2d36e55b078
Mikhail Kosarev <michael.kosarev@gmail.com>
2022-09-30 15:43:20 +0200

    create UILabel and UIStackView extensions

---
+++ WeatherApp-MVP/Extensions/UILabel + Extensions.swift
@@ -0,0 +1,30 @@
+//
+//  UILabel + Extensions.swift
+//  WeatherApp-MVP
+//
+//  Created by Mikhail on 30.09.2022.
+//
+
+import UIKit
+
+extension UILabel {
+    static func makeDetailWeatherCellTitleLabel(text: String = "Temperature max") -> UILabel {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = text
+        label.textColor = .label
+        label.textAlignment = .left
+        label.font = .preferredFont(forTextStyle: .subheadline)
+        return label
+    }
+    
+    static func makeDetailWeatherCellValueLabel(text: String = "14°C") -> UILabel {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = text
+        label.textColor = .label
+        label.textAlignment = .right
+        label.font = .preferredFont(forTextStyle: .title3)
+        return label
+    }
+}
