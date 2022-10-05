ffa75c853ba1f9f057588736c50d83dbe73e9fbd
Mikhail Kosarev <michael.kosarev@gmail.com>
2022-10-05 10:57:37 +0200

    add an invalid cityName alert to the CurrentWeather screen

--- WeatherApp-MVP/CurrentWeatherModule/Presenter/CurrentWeatherPresenter.swift
+++ WeatherApp-MVP/CurrentWeatherModule/Presenter/CurrentWeatherPresenter.swift
@@ -10,12 +10,13 @@ import UIKit
 // an input protocol
 protocol CurrentWeatherViewProtocol: UIViewController {
     func reloadWeather(city: String, degree: String, condition: String)
+    func presentAlert(alert: UIAlertController)
 }
 
 // an output protocol
 protocol CurrentWeatherPresenterProtocol: AnyObject {
     init(view: CurrentWeatherViewProtocol, networkService: NetworkServiceProtocol)
-    
+    func makeAlert() -> UIAlertController
     func getCurrentWeather(for cityName: String)
 }
 
@@ -30,7 +31,17 @@ final class CurrentWeatherPresenter: CurrentWeatherPresenterProtocol {
         self.networkService = networkService
     }
     
-    func getCurrentWeather(for cityName: String) {
+    // MARK: - Private methods
+    internal func makeAlert() -> UIAlertController {
+        let alert = UIAlertController(title: "Error", message: "Please type a valid city name", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .destructive)
+        alert.addAction(okAction)
+        return alert
+    }
+    
+    
+    // MARK: - Public methods
+    public func getCurrentWeather(for cityName: String) {
         networkService.getCurrentWeather(cityName: cityName) { [weak self]  result in
             switch result {
             case .success(let currentWeatherData):
@@ -40,12 +51,17 @@ final class CurrentWeatherPresenter: CurrentWeatherPresenterProtocol {
                                                        conditionId: currentWeatherData.weather[0].id)
                 // reload view
                 DispatchQueue.main.async {
-                    
                     self?.view?.reloadWeather(city: currentWeather.cityName,
                                               degree: currentWeather.temperatureString,
                                               condition: currentWeather.conditionName)
                 }
             case .failure(let error):
+                if let alert = self?.makeAlert() {
+                    DispatchQueue.main.async {
+                        self?.view?.presentAlert(alert: alert)
+                    }
+
+                }
                 print(error)
             }
         }
